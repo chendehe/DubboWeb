57625875c591b3c057523e677d61a39e380f9014
chendehe <chendehe22@163.com>
2019-08-12 09:34:41 +0800

    Dubbo SPI 目录优先级验证

---
+++ dubbo-spi/src/main/java/com/chendehe/web/dspi/PrintServiceDubboImpl.java
@@ -0,0 +1,12 @@
+package com.chendehe.web.dspi;
+
+/**
+ * @author CDH
+ * @since 2019.8.11 011 22:03
+ */
+public class PrintServiceDubboImpl implements PrintService {
+    @Override
+    public void printInfo() {
+        System.out.println("Hello Dubbo spi Dubbo");
+    }
+}
